3044cfd20f9b7711186a9a3ec970cbf7a7c8acb1
Sybren <92291687+sybrenkeizer@users.noreply.github.com>
2022-04-04 09:35:07 +0530

    Add a slightly cleaner version of function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(start, end) {
   let range = [];
   if (start < 0 || end < 0) {
     return `ERROR`;
-  } else if (typeof start !== `number` || typeof end !== `number`) {
+  } if (typeof start !== `number` || typeof end !== `number`) {
     return `ERROR`;
   } else if (start < end) {
     for (let i = start; i <= end; i++) {
@@ -22,3 +22,26 @@ module.exports = sumAll;
 
 
 sumAll(4, '2');
+
+/*
+
+*** CLEANER VERSION ***
+
+const sumAll = function(start, end) {
+  let range = [];
+  if (start < 0 || end < 0) return `ERROR`;
+  if (typeof start !== `number` || typeof end !== `number`) return `ERROR`;
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      range.push(i);
+    }
+  } else if (start > end) {
+    for (let i = start; i >= end; i--) {
+      range.push(i);
+    }
+  }
+  return range.reduce((previous, current) =>
+    previous + current, 0);
+};
+
+*/
